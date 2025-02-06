dbc7a88e6c395b71f7d05b1e83ab07d86bb9ecce
Ethan Eldridge <ejayeldridge@gmail.com>
2025-02-05 23:57:20 -0500

    Move lib to cli file
    I want to move each thing to its own module I think. Rather than
    everything stuffed into a lib.

--- sentimentuber/src/cli.rs
+++ sentimentuber/src/cli.rs
@@ -9,26 +9,24 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
-/// Run the program with the given configuration
-///
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("{:?}", config);
-    Ok(())
-}
-
 #[derive(Debug)]
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct Config {
+
+    /// Path to localvocal (or similar) transcript file
     #[arg(long = "file", value_name = "FILE")]
     pub input_text_file_path: PathBuf,
 
+    /// IP address of OBS websocket
     #[arg(long = "ip")]
     pub obs_ip: String,
 
+    /// Password of OBS websocket
     #[arg(short = 'p', long = "password")]
     pub obs_password: String,
 
+    /// Port for OBS websocket
     #[arg(long = "port", default_value_t = 4455)]
     pub obs_port: u16
 }
@@ -37,14 +35,4 @@ impl Config {
     pub fn parse_env() -> Config {
         Config::parse()
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn we_have_boilerplate() {
-        assert!(true);
-    }   
 }
--- sentimentuber/src/main.rs
+++ sentimentuber/src/main.rs
@@ -1,10 +1,19 @@
+//! # SentimentTuber
+//!
+//! `sentimenttuber` is a tool to read in a text file produced by 
+//! OBS's localvocal plugin and determine an image to show for an
+//! avatar based on keywords and sentiment.
+
+mod cli;
+
+use cli::Config;
+
 use notify::{Event, RecursiveMode, Result, Watcher};
 use std::sync::mpsc;
 use std::path::Path;
 use std::fs;
 use std::process;
 use std::env;
-use sentimentuber::Config;
 
 use vader_sentiment;
 use obws::Client;
@@ -39,18 +48,22 @@ async fn main() -> anyhow::Result<()> {
     // pre-fetch the image container we'll be tweaking
     let image_source_id = get_image_scene_item(&client).await?;
 
-    let (tx, rx) = mpsc::channel::<Result<Event>>();
-    let mut watcher = notify::recommended_watcher(tx)?;
+    let (sender, receiver) = mpsc::channel::<Result<Event>>();
+    let mut watcher = notify::recommended_watcher(sender)?;
     watcher.watch(path, RecursiveMode::Recursive)?;
     // Block forever, printing out events as they come in
-    for res in rx {
+    for res in receiver {
         match res {
             Ok(event) => {
+                // TODO:
+                // probably take a bit of file at a time, append to a buffer
+                // and then analyze the buffer rather than do one bit at a time
+                // Also we should keep track of how long its been since data 
+                // came in, and then we could use that to do mouth flaps
                 let s = get_data_from_file(path);
                 // we'll do something with the score later.
                 let emotional_state = get_emotional_state(&s, &analyzer);
                 let image_to_show = state_to_image_file.get(&emotional_state).unwrap();
-                println!("Should show {:?}", image_to_show);
                 swap_obs_image_to(&image_source_id, &image_to_show, &client).await?;
             },
             Err(e) => println!("watch error: {:?}", e),
@@ -122,6 +135,10 @@ fn get_emotion_to_image_map() -> HashMap<EmotionalState, &'static str> {
     ])
 }
 
+// TODO:
+// probably make this file based rather than compile code based to some extent
+// define things like keyword foo -> Bla and letting rules cascade would be good
+// not to mention we'll need to think about blinking or similar things one should do regularly.
 fn get_emotional_state(sentence: &String, analyzer: &vader_sentiment::SentimentIntensityAnalyzer) -> EmotionalState {
     if sentence.contains("good job") {
         return EmotionalState::ThumbsUp
