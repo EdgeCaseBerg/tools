2c0f5b553604367460cd726e8237b288eb9e6beb
Ethan Eldridge <ejayeldridge@gmail.com>
2025-02-10 08:11:25 -0500

    Debounce events + a bit of import cleanup

--- sentimentuber/src/cli.rs
+++ sentimentuber/src/cli.rs
@@ -1,19 +1,15 @@
 //! # SentimentTuber
 //!
-//! `sentimenttuber` is a tool to read in a text file produced by 
+//! `sentimenttuber` is a tool to read in a text file produced by
 //! OBS's localvocal plugin and determine an image to show for an
 //! avatar based on keywords and sentiment.
 
-use std::error::Error;
 use std::path::PathBuf;
-
 use clap::Parser;
 
-#[derive(Debug)]
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 pub struct Config {
-
     /// Path to localvocal (or similar) transcript file
     #[arg(long = "file", value_name = "FILE")]
     pub input_text_file_path: PathBuf,
@@ -28,7 +24,11 @@ pub struct Config {
 
     /// Port for OBS websocket
     #[arg(long = "port", default_value_t = 4455)]
-    pub obs_port: u16
+    pub obs_port: u16,
+
+    /// Milliseconds to aggregate events from the local file system for changes to the transcript file
+    #[arg(short = 'd',  long = "debounce_milliseconds", default_value_t = 50)]
+    pub event_debouncing_duration_ms: u64,
 }
 
 impl Config {
--- sentimentuber/src/main.rs
+++ sentimentuber/src/main.rs
@@ -1,6 +1,6 @@
 //! # SentimentTuber
 //!
-//! `sentimenttuber` is a tool to read in a text file produced by 
+//! `sentimenttuber` is a tool to read in a text file produced by
 //! OBS's localvocal plugin and determine an image to show for an
 //! avatar based on keywords and sentiment.
 
@@ -8,49 +8,43 @@ mod cli;
 
 use cli::Config;
 
-use notify::{Event, RecursiveMode, Result, Watcher};
-use std::sync::mpsc;
-use std::path::Path;
+use notify::RecursiveMode;
+use notify_debouncer_mini::new_debouncer;
 use std::fs;
-use std::process;
-use std::env;
+use std::path::Path;
+use std::sync::mpsc;
+use std::time::Duration;
 
-use vader_sentiment;
-use obws::Client;
-use obws::responses::scene_items::SceneItem;
+use obws::requests::inputs::SetSettings;
 use obws::requests::scene_items::Source;
 use obws::responses::sources::SourceId;
-use obws::requests::inputs::SetSettings;
+use obws::Client;
+use vader_sentiment;
 
-use tokio;
 use anyhow;
-use std::collections::HashMap;
 use serde_json::json;
+use std::collections::HashMap;
+use tokio;
 
 // TODO: Cite https://github.com/ckw017/vader-sentiment-rust?tab=readme-ov-file#citation-information
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let config = Config::parse_env();
-    println!("{:?}", config);
-
     let path = config.input_text_file_path.as_path();
     let ip = config.obs_ip;
     let password = config.obs_password;
     let port = config.obs_port;
+    let debounce_milli = config.event_debouncing_duration_ms;
 
     let analyzer = vader_sentiment::SentimentIntensityAnalyzer::new();
-
     let state_to_image_file = get_emotion_to_image_map();
-
     let client = Client::connect(ip, port, Some(password)).await?;
-
-    // pre-fetch the image container we'll be tweaking
     let image_source_id = get_image_scene_item(&client).await?;
 
-    let (sender, receiver) = mpsc::channel::<Result<Event>>();
-    let mut watcher = notify::recommended_watcher(sender)?;
-    watcher.watch(path, RecursiveMode::Recursive)?;
+    let (sender, receiver) = mpsc::channel();
+    let mut debouncer = new_debouncer(Duration::from_millis(debounce_milli), sender).unwrap();
+    debouncer.watcher().watch(path, RecursiveMode::Recursive).unwrap();
     // Block forever, printing out events as they come in
     for res in receiver {
         match res {
@@ -58,15 +52,15 @@ async fn main() -> anyhow::Result<()> {
                 // TODO:
                 // probably take a bit of file at a time, append to a buffer
                 // and then analyze the buffer rather than do one bit at a time
-                // Also we should keep track of how long its been since data 
+                // Also we should keep track of how long its been since data
                 // came in, and then we could use that to do mouth flaps
                 let s = get_data_from_file(path);
                 // we'll do something with the score later.
                 let emotional_state = get_emotional_state(&s, &analyzer);
                 let image_to_show = state_to_image_file.get(&emotional_state).unwrap();
                 swap_obs_image_to(&image_source_id, &image_to_show, &client).await?;
-            },
-            Err(e) => println!("watch error: {:?}", e),
+            }
+            Err(e) => eprintln!("watch error: {:?}", e),
         }
     }
 
@@ -74,54 +68,70 @@ async fn main() -> anyhow::Result<()> {
 }
 
 fn get_data_from_file(path: &Path) -> String {
-    let s = fs::read_to_string(path).expect("could not get text data from file shared with localvocal");
-    return s
+    let s =
+        fs::read_to_string(path).expect("could not get text data from file shared with localvocal");
+    return s;
 }
 
 async fn get_image_scene_item(client: &Client) -> anyhow::Result<SourceId> {
     let scenes_struct = client.scenes().list().await?;
-    let test_scene = scenes_struct.scenes.iter().find(|scene| {
-        scene.id.name.contains("SentimentTuber")
-    }).expect("Could not find OBS scene by name");
+    let test_scene = scenes_struct
+        .scenes
+        .iter()
+        .find(|scene| scene.id.name.contains("SentimentTuber"))
+        .expect("Could not find OBS scene by name");
 
-    let items_in_scene = client.scene_items().list(test_scene.id.clone().into()).await?;
-    let image_source = items_in_scene.iter().find(|item| {
-        // TODO: use a better name than "Image" obviously.
-        item.source_name.contains("Image")
-    }).expect("No image source found in OBS scene for the avatar");
+    let items_in_scene = client
+        .scene_items()
+        .list(test_scene.id.clone().into())
+        .await?;
+    let image_source = items_in_scene
+        .iter()
+        .find(|item| {
+            // TODO: use a better name than "Image" obviously.
+            item.source_name.contains("Image")
+        })
+        .expect("No image source found in OBS scene for the avatar");
 
-    let source_id = client.scene_items().source(
-        Source {
+    let source_id = client
+        .scene_items()
+        .source(Source {
             scene: test_scene.id.clone().into(),
-            item_id: image_source.id.clone().into()
-        }
-    ).await?;
+            item_id: image_source.id.clone().into(),
+        })
+        .await?;
 
     Ok(source_id)
 }
 
-async fn swap_obs_image_to(source_id: &SourceId, new_file_path: &str, client: &Client) -> anyhow::Result<()> {
+async fn swap_obs_image_to(
+    source_id: &SourceId,
+    new_file_path: &str,
+    client: &Client,
+) -> anyhow::Result<()> {
     let path = Path::new(new_file_path);
     let absolute = Path::canonicalize(path)?;
     let setting = json!({"file": absolute});
-    client.inputs().set_settings(SetSettings {
-        input: (&*source_id.name).into(),
-        settings: &setting,
-        overlay: Some(true)
-    }).await?;
+    client
+        .inputs()
+        .set_settings(SetSettings {
+            input: (&*source_id.name).into(),
+            settings: &setting,
+            overlay: Some(true),
+        })
+        .await?;
 
     Ok(())
 }
 
-#[derive(Debug)]
-#[derive(Hash, Eq, PartialEq)]
+#[derive(Debug, Hash, Eq, PartialEq)]
 enum EmotionalState {
     Neutral,
     Mad,
     MakingAPromise,
     Sad,
     Smug,
-    ThumbsUp
+    ThumbsUp,
 }
 
 fn get_emotion_to_image_map() -> HashMap<EmotionalState, &'static str> {
@@ -131,7 +141,7 @@ fn get_emotion_to_image_map() -> HashMap<EmotionalState, &'static str> {
         (EmotionalState::MakingAPromise, "./data/promise.png"),
         (EmotionalState::Sad, "./data/sad.png"),
         (EmotionalState::Smug, "./data/smug.png"),
-        (EmotionalState::ThumbsUp, "./data/thumbsup.png")
+        (EmotionalState::ThumbsUp, "./data/thumbsup.png"),
     ])
 }
 
@@ -139,19 +149,22 @@ fn get_emotion_to_image_map() -> HashMap<EmotionalState, &'static str> {
 // probably make this file based rather than compile code based to some extent
 // define things like keyword foo -> Bla and letting rules cascade would be good
 // not to mention we'll need to think about blinking or similar things one should do regularly.
-fn get_emotional_state(sentence: &String, analyzer: &vader_sentiment::SentimentIntensityAnalyzer) -> EmotionalState {
+fn get_emotional_state(
+    sentence: &String,
+    analyzer: &vader_sentiment::SentimentIntensityAnalyzer,
+) -> EmotionalState {
     if sentence.contains("good job") {
-        return EmotionalState::ThumbsUp
+        return EmotionalState::ThumbsUp;
     }
     if sentence.contains("promise") {
-        return EmotionalState::MakingAPromise
+        return EmotionalState::MakingAPromise;
     }
     if sentence.contains("I'm the best") || sentence.contains("I am the best") {
-        return EmotionalState::Smug
+        return EmotionalState::Smug;
     }
 
     if sentence.contains("bummer") {
-        return EmotionalState::Sad
+        return EmotionalState::Sad;
     }
 
     let scores = analyzer.polarity_scores(&sentence);
@@ -161,7 +174,7 @@ fn get_emotional_state(sentence: &String, analyzer: &vader_sentiment::SentimentI
     let neutral = scores.get("neu").unwrap_or_else(|| &0.0);
 
     if positive < negative && neutral < negative {
-        return EmotionalState::Mad
+        return EmotionalState::Mad;
     }
 
     EmotionalState::Neutral
