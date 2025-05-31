daaec5ed4875555ef0c047ca9557f50ab931b57a
Ethan Eldridge <ejayeldridge@gmail.com>
2025-05-31 17:37:10 -0400

    Add constants for screen size from gamepad example

--- gamepad/src/main.rs
+++ gamepad/src/main.rs
@@ -4,10 +4,13 @@ const XBOX_ALIAS_1: &str = "xbox";
 const XBOX_ALIAS_2: &str = "xbox";
 const PS_ALIAS: &str = "playstation";
 
+const screen_width: u32 = 800;
+const screen_height: u32 = 450;
+
 fn main() {
     let (mut rl, thread) = raylib::init()
-        .size(640, 480)
-        .title("Hello, World")
+        .size(screen_width, screen_height)
+        .title("Game Pad output")
         .build();
      
     while !rl.window_should_close() {
