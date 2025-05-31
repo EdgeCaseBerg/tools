cc3214c8be8e6590cf4c93da069df0b680c38f8c
Ethan Eldridge <ejayeldridge@gmail.com>
2025-05-31 17:59:32 -0400

    test that gamepad is detected (it is)

--- gamepad/src/main.rs
+++ gamepad/src/main.rs
@@ -27,10 +27,19 @@ fn main() {
 
     let xbox_texture = rl.load_texture(&thread, "resources/xbox.png").expect("Cannot run program if gamepad texture (xbox) missing");
     let ps3_texture  = rl.load_texture(&thread, "resources/ps3.png").expect("Cannot run program if gamepad texture (ps3) missing");
+
+    // Original example uses left and right to swap pad to display using this.
+    let gamepad_to_display = 0;
      
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
          
+        if d.is_gamepad_available(gamepad_to_display) {
+            d.draw_text(&format!("Gamepad: {gamepad_to_display}"), 60, 60, 20, Color::BLACK);            
+        } else {
+            d.draw_text(&format!("No Gamepad: {gamepad_to_display}"), 60, 60, 20, Color::BLACK);            
+        }
+
         d.clear_background(Color::WHITE);
         d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
     }
