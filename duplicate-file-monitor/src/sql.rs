989ac83b11db77dbc1194eed43d4635c11e28040
Ethan Eldridge <ejayeldridge@gmail.com>
2025-07-20 11:53:47 -0400

    Add unit tests against sqlite code
    They're all passing, so the issues I observed that led the previous
    revert are likely in the other methods and not a direct result of the
    sqlite db's queries and such

--- duplicate-file-monitor/src/sql.rs
+++ duplicate-file-monitor/src/sql.rs
@@ -149,3 +149,83 @@ pub fn add_hash_to_db_test(conn: &Connection, file_path: PathBuf) {
 
     reset_all_data(conn);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+ 
+    static mut TEST_DB_NO: u32 = 0;
+
+    fn open_test_database() -> Connection {
+        let connection;
+
+        unsafe {
+            TEST_DB_NO += 1;
+            let filename = format!("test_{TEST_DB_NO}.sqlite.db");
+            let _ = fs::remove_file(&filename);
+            connection = Connection::open(filename).expect("Cannot open database for test");
+        };
+        initialize(&connection);
+        connection
+    }
+
+    #[test]
+    fn count_of_non_existing_hash_should_be_0() {
+        let connection = open_test_database();
+        let count_of_nothingness = count_of_same_hash(&connection, 196248234750);
+        let marking_time_waiting_for_death = 0;
+        assert_eq!(count_of_nothingness, marking_time_waiting_for_death);
+    }
+
+    #[test]
+    fn count_of_existing_hash_should_be_n() {
+        let connection = open_test_database();
+        let hash = 123456789;
+        let path = "12345689";
+        // Insert something other than the one we're testing too
+        insert_file_hash(&connection, 9876543211, "987654321");
+        insert_file_hash(&connection, hash, path);
+        let begin_instrumentality = count_of_same_hash(&connection, hash);
+        let hall_of_goff = 1;
+        assert_eq!(begin_instrumentality, hall_of_goff);
+        insert_file_hash(&connection, hash, path);
+        let rejoicing_of_the_masses = count_of_same_hash(&connection, hash);
+        assert_eq!(rejoicing_of_the_masses, 2);
+    }
+
+    #[test]
+    fn select_dupes_based_on_filepath_hash() {
+        let connection = open_test_database();
+        // Insert something other than the one we're testing too
+        insert_file_hash(&connection, 9876543211, "987654321");
+        let hash = 1234567;
+        let path = "hellothere";
+        for i in 0..10 {
+            insert_file_hash(&connection, hash, path);
+        }
+        let there_should_be_10_dupes = dups_by_file(&connection, path);
+        assert_eq!(there_should_be_10_dupes.len(), 10);
+        for (db_hash, db_path) in there_should_be_10_dupes {
+            assert_eq!(hash, db_hash.parse::<u64>().expect("could not parse db u64 string to u64"));
+            assert_eq!(path, db_path);
+        }
+    }
+
+    #[test]
+    fn can_delete_from_database_for_matches() {
+        let connection = open_test_database();
+        // Insert something other than the one we're testing too
+        insert_file_hash(&connection, 9876543211, "987654321");
+        let hash = 1234567;
+        let path = "hellothere";
+        for i in 0..2 {
+            insert_file_hash(&connection, hash, path);
+        }
+        let there_should_be_2_dupes = dups_by_file(&connection, path);
+        assert_eq!(there_should_be_2_dupes.len(), 2);
+        let deleted = delete_all_matching(&connection, hash, path);
+        assert_eq!(deleted, 2);
+        let should_be_zero = count_of_same_hash(&connection, hash);
+        assert_eq!(should_be_zero, 0);
+    }
+}
